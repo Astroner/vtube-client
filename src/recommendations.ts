5fce2942c9dd56ea58f36958c2f2daf5196be2bf
Evgeny Nikiforov <enikiforov@devexperts.com>
2023-08-12 20:48:25 +0300

    youtube recommendations

--- src/recommendations.ts
+++ src/recommendations.ts
@@ -1,11 +1,21 @@
-import { MusicCategories } from "./responses";
+import { MusicCategories, Recommendation } from "./responses";
 import { PrivateAPI } from "./private-api.class";
 
 export class Recommendations extends PrivateAPI {
+    async youtube() {
+        const { data } = await this.axios.get<Recommendation[]>(`/recommendations/youtube/`, {
+            headers: {
+                Authorization: this.token,
+            },
+        });
+        
+        return data;
+    }
+
     async music() {
         const { data } = await this.axios.get<MusicCategories>(`/recommendations/music/`, {
             headers: {
-              Authorization: this.token,
+                Authorization: this.token,
             },
         });
         
